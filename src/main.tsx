f247a4c698f066828a0514e00999441599bf3fa1
Yuri Varshavsky <yuri.varshavsky@leetio.org>
2025-03-19 17:15:55 +0200

    Use HashRouter instead of BrowserRouter in main.tsx
    Replaced BrowserRouter with HashRouter to address routing compatibility issues in specific environments. Also formatted the code to use consistent double quotes for imports and JSX attributes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import './shared/styles/main.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./shared/styles/main.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
